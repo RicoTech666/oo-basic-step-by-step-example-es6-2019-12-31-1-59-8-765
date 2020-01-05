2f2c5557b1569703a17fb0ec47179c38e841655e
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-05 21:36:22 +0800

    [Jiachen] feat: class for practice 1

--- main/practice_2/student.js
+++ main/practice_2/student.js
@@ -1,1 +1,11 @@
+class Student extends Person {
+	constructor(name, age, klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+	introduce() {
+		return `I am a ${Student.name}. I am at Class ${this.klass}.`;
+	}
+}
 
+module.exports = Student;
