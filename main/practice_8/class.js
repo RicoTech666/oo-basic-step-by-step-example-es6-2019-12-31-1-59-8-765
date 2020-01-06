2c9acf12f7c1d60a7bd8fbee417be41cd674710c
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-06 12:05:11 +0800

    [Jiachen] feat: add classes for practice 8

--- main/practice_8/class.js
+++ main/practice_8/class.js
@@ -1,1 +1,15 @@
+class Class {
+	constructor(number) {
+		this.number = number;
+	}
+	assignLeader(studentObj) {
+		if (studentObj.klass.number === this.number) {
+			this.leader = studentObj;
+		}
+	}
+	getDisplayName() {
+		return `Class ${this.number}`;
+	}
+}
 
+module.exports = Class;
