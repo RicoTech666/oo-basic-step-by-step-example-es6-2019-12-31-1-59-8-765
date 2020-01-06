b3ce91e0c647c3f8998571b83ae58d711a31d28f
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-06 12:54:07 +0800

    [Jiachen] feat: add classes for practice 10

--- main/practice_10/class.js
+++ main/practice_10/class.js
@@ -1,1 +1,20 @@
+class Class {
+	constructor(number) {
+		this.number = number;
+	}
+	assignLeader(studentObj) {
+		if (studentObj.klass.number === this.number) {
+			this.leader = studentObj;
+		} else {
+			console.log(`It is not one of us.`);
+		}
+	}
+	getDisplayName() {
+		return `Class ${this.number}`;
+	}
+	appendMember(studentObj) {
+		studentObj.klass = this;
+	}
+}
 
+module.exports = Class;
--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,27 @@
+import Person from "./person.js";
 
+class Teacher extends Person {
+	constructor(id, name, age, klasses) {
+		super(id, name, age);
+		this.klasses = klasses;
+	}
+	introduce() {
+		if (this.klasses) {
+			return `${super.introduce()} I am a Teacher. I teach Class ${this.klasses
+				.map(elem => elem.number)
+				.toString()
+				.replace(",", ", ")}.`;
+		} else {
+			return `${super.introduce()} I am a Teacher. I teach No Class.`;
+		}
+	}
+	introduceWith(student) {
+		if (student.klass === this.klass) {
+			return `${super.introduce()} I am a Teacher. I teach ${student.name}.`;
+		} else {
+			return `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`;
+		}
+	}
+}
+
+module.exports = Teacher;
